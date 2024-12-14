805b00ae3625b1d2547ee621ca6718b95073a909
dragonhunt02 <jackhunt43@libero.it>
2024-12-14 19:48:45 +0100

    Update entrypoint.sh

--- docker/itch-io/entrypoint.sh
+++ docker/itch-io/entrypoint.sh
@@ -7,6 +7,7 @@ tree -a -L 2 .
 cd ${INPUT_FILEPATH}
 for file in *; do \
    CHANNEL=$( grep -E -io "windows|linux|mac|android" ); \
-   echo "channel ${CHANNEL}"
+   echo "channel ${CHANNEL}";
+   echo $( grep -E -io "windows|linux|mac|android" );
    echo "Uploading ${file} to ${INPUT_ITCHIO_PROJECT} for platform ${CHANNEL}..."; \
 done
