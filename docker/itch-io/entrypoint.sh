56ab2604c64e9c66c65f14f337530b3deab9e035
dragonhunt02 <jackhunt43@libero.it>
2024-12-14 20:27:34 +0100

    Update entrypoint.sh

--- docker/itch-io/entrypoint.sh
+++ docker/itch-io/entrypoint.sh
@@ -6,7 +6,7 @@ tree -a -L 2 .
 
 cd ${INPUT_FILEPATH}
 for file in *; do \
-   CHANNEL=$( echo "${file}" | cut -d '_' -f3 | tr '[:upper:]' '[:lower:]'); \
+   CHANNEL=$( basename ${file} | cut -d '_' -f3 | tr '[:upper:]' '[:lower:]'); \
    echo "channel ${CHANNEL}"; \
    echo "Uploading ${file} to ${INPUT_ITCHIO_PROJECT} for platform ${CHANNEL}..."; \
 done
