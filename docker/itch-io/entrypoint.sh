70b082edb099d2d02185b54a7113546dfbceb3d6
dragonhunt02 <jackhunt43@libero.it>
2024-12-14 20:57:46 +0100

    Update entrypoint.sh

--- docker/itch-io/entrypoint.sh
+++ docker/itch-io/entrypoint.sh
@@ -4,6 +4,6 @@ echo "Workspace release folder: ${GITHUB_WORKSPACE}/${INPUT_FILEPATH}"
 
 cd ${INPUT_FILEPATH}
 for file in *; do
-   CHANNEL=$( echo "${file%.*}" | cut -d '_' -f3 | tr '[:upper:]' '[:lower:]');
+   CHANNEL=$( echo "${file}" | cut -d '_' -f3 | cut -d '.' -f1 | tr '[:upper:]' '[:lower:]');
    echo "Uploading ${file} to ${INPUT_ITCHIO_PROJECT} for platform ${CHANNEL}...";
 done
