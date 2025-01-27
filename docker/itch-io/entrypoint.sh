3a70def767d8435db36f1d27cfc6bd2df3046aba
dragonhunt02 <jackhunt43@libero.it>
2025-01-27 23:30:10 +0100

    Update entrypoint.sh

--- docker/itch-io/entrypoint.sh
+++ docker/itch-io/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Github Action sets inputs when running image
+if [ -z "$BUTLER_API_KEY" ]; then
+    BUTLER_API_KEY=$INPUT_API_KEY
+fi
+
 echo "Workspace release folder: ${GITHUB_WORKSPACE}/${INPUT_FILEPATH}"
 echo "Butler version:"
 butler -V
@@ -8,5 +13,5 @@ cd ${INPUT_FILEPATH}
 for file in *; do
    CHANNEL=$( echo "${file}" | cut -d '_' -f3 | cut -d '.' -f1 | tr '[:upper:]' '[:lower:]');
    echo "Uploading ${file} to ${INPUT_ITCHIO_PROJECT} for platform ${CHANNEL}...";
-   butler push ${file} ${INPUT_ITCHIO_PROJECT}:${CHANNEL} --userversion ${INPUT_RELEASE_VERSION};
+   # butler push ${file} ${INPUT_ITCHIO_PROJECT}:${CHANNEL} --userversion ${INPUT_RELEASE_VERSION};
 done
