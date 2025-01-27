12cb22fbbec9cf5a2341f38780c35465583e4604
dragonhunt02 <jackhunt43@libero.it>
2025-01-27 21:22:00 +0000

    Add automated build system

---
+++ docker/itch-io/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Workspace release folder: ${GITHUB_WORKSPACE}/${INPUT_FILEPATH}"
+echo "Butler version:"
+butler -V
+
+cd ${INPUT_FILEPATH}
+for file in *; do
+   CHANNEL=$( echo "${file}" | cut -d '_' -f3 | cut -d '.' -f1 | tr '[:upper:]' '[:lower:]');
+   echo "Uploading ${file} to ${INPUT_ITCHIO_PROJECT} for platform ${CHANNEL}...";
+   butler push ${file} ${INPUT_ITCHIO_PROJECT}:${CHANNEL} --userversion ${INPUT_RELEASE_VERSION};
+done
