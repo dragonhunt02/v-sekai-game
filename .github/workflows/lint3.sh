facb0cb4c6c847dd5cc59ce362086d4850aecbd7
dragonhunt02 <jackhunt43@libero.it>
2025-02-18 17:47:09 +0100

    Update lint3.sh

--- .github/workflows/lint3.sh
+++ .github/workflows/lint3.sh
@@ -20,8 +20,8 @@ echo "$PATTERNS"
 
 # Decision https://github.com/V-Sekai/v-sekai-game/issues/474#issuecomment-2603661420
 # Forbid assert()
-echo "find . -type f -regextype egrep -name '*.gd' -and -not -regex \"${PATTERNS[@]}\" -exec echo {} \;"
-matches=$( bash -c "find . -type f -regextype egrep -name '*.gd' -and -not -regex \"${PATTERNS[@]}\" -exec echo {} \;" )
+echo "find . -type f -regextype egrep -name '*.gd' -and -not -regex \"${PATTERNS}\" -exec echo {} \;"
+matches=$( bash -c "find . -type f -regextype egrep -name '*.gd' -and -not -regex \"${PATTERNS}\" -exec echo {} \;" )
 if [ -n "$matches" ]; then
     echo 'Linter: "assert()" usage is forbidden (assert checks are skipped in release versions causing potential undefined behaviour)';
     echo 'Linter: use constructs like "if not ...: push_error(...); return" instead';
