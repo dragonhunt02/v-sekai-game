68feb65ef3c6a730b41674b25e4bc6553609b7c0
dragonhunt02 <jackhunt43@libero.it>
2025-02-18 14:09:36 +0100

    Create lint2.sh

---
+++ .github/workflows/lint2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+EXCLUDE="
+./addons/vrm/.*
+"
+
+export DIR_PATHS2=()
+# Separate by newline
+OLD=$IFS
+IFS='
+'
+set -f
+for DIRE in $EXCLUDE; do
+    DIR_PATHS2+=("-not -regex \"$DIRE\"")
+done
+set +f
+
+IFS=$OLD
+
+echo "Linter: Start custom linter...";
+match_error=false;
+
+# Decision https://github.com/V-Sekai/v-sekai-game/issues/474#issuecomment-2603661420
+# Forbid assert()
+matches=$( bash -c "find . -type f -name '*.gd' ${DIR_PATHS2[@]} -exec echo {} \;" )
+if [ -n "$matches" ]; then
+    echo 'Linter: "assert()" usage is forbidden (assert checks are skipped in release versions causing potential undefined behaviour)';
+    echo 'Linter: use constructs like "if not ...: push_error(...); return" instead';
+    echo "$matches";
+    match_error=true;
+fi
+
+# Decision https://github.com/V-Sekai/v-sekai-game/issues/475#issue-2802564439
+# Forbid printerr()
+matches=$( find . -type f -name "*.gd" $DIR_PATHS -exec grep -nH 'printerr(' {} \; )
+if [ -n "$matches" ]; then
+    echo 'Linter: "printerr()" usage is forbidden (error output won'\''t be shown in Godot Debug panel)';
+    echo 'Linter: use "push_error()" instead';
+    echo "$matches";
+    match_error=true;
+fi
+
+if [ "$match_error" == 'true' ]; then
+    echo 'Linter: Solve errors and run again';
+    exit 1;
+else
+    echo "Linter: All checks passed!"
+fi
