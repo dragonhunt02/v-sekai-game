ff1fc13ce3210269083834a6810bb65784c3f041
dragonhunt02 <jackhunt43@libero.it>
2025-02-18 11:06:28 +0100

    Update lint.sh

--- .github/workflows/lint.sh
+++ .github/workflows/lint.sh
@@ -29,7 +29,7 @@ match_error=false;
 
 # Decision https://github.com/V-Sekai/v-sekai-game/issues/474#issuecomment-2603661420
 # Forbid assert()
-matches=$( find . -type f -name "*.gd" $DIR_PATHS -exec grep -nH 'assert(' {} \; )
+matches=$( find . -type f -name "*.gd" ${DIR_PATHS}-exec grep -nH 'assert(' {} \; )
 #$( grep -rn --include='*.gd' --exclude-dir="./addons/vrm/" -e 'assert(' . || true )
 if [ -n "$matches" ]; then
     echo 'Linter: "assert()" usage is forbidden (assert checks are skipped in release versions causing potential undefined behaviour)';
