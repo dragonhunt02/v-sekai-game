272f32c51a059ce666603fbbf85f284298e07212
dragonhunt02 <jackhunt43@libero.it>
2025-02-18 11:40:09 +0100

    Update lint.sh

--- .github/workflows/lint.sh
+++ .github/workflows/lint.sh
@@ -30,7 +30,7 @@ match_error=false;
 
 # Decision https://github.com/V-Sekai/v-sekai-game/issues/474#issuecomment-2603661420
 # Forbid assert()
-matches=$( find . -type d ${DIR_PATHS} -exec grep -nH -include="*.gd" 'assert(' {} \; )
+matches=$( find . -type d ${DIR_PATHS} -exec grep -nH --include="*.gd" 'assert(' {} \; )
 #matches=$( find . -type f -name "*.gd" ${DIR_PATHS} -exec grep -nH 'assert(' {} \; )
 #$( grep -rn --include='*.gd' --exclude-dir="./addons/vrm/" -e 'assert(' . || true )
 if [ -n "$matches" ]; then
