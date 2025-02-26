0f9a550a167b889d0e8801a678ca128b50243689
dragonhunt02 <jackhunt43@libero.it>
2025-02-27 00:58:53 +0100

    Update godot_uro_helper.gd

--- addons/godot_uro/godot_uro_helper.gd
+++ addons/godot_uro/godot_uro_helper.gd
@@ -50,11 +50,7 @@ const DEFAULT_ACCOUNT_USERNAME = "UNKNOWN_USERNAME"
 const DEFAULT_ACCOUNT_DISPLAY_NAME = "UNKNOWN_DISPLAY_NAME"
 const UNTITLED_SHARD = "UNTITLED_SHARD"
 const UNKNOWN_MAP = "UNKNOWN_MAP"
-
-# NEW API
-const NEW_PROFILE_PATH = "/session"
-const NEW_REGISTRATION_PATH = "/signup"
-const NEW_LOGIN_PATH = "/login"
+const SIGNUP_API_KEY = "eNoZ4kXHgT0z9ZTYGsq7eE0rQYvR6YBi"
 
 
 static func get_string_for_requester_code(p_requester_code: int) -> String:
