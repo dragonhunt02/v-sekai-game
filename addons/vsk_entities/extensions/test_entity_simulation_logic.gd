eacbed1cca15e9f5142d7cdce1899423cccb037f
dragonhunt02 <jackhunt43@libero.it>
2025-05-29 09:15:42 +0200

    Update test_entity_simulation_logic.gd

--- addons/vsk_entities/extensions/test_entity_simulation_logic.gd
+++ addons/vsk_entities/extensions/test_entity_simulation_logic.gd
@@ -1,8 +1,10 @@
 @tool
 extends "res://addons/entity_manager/node_3d_simulation_logic.gd"
 
-# const interactable_prop_const = preload("res://addons/vsk_entities/vsk_interactable_prop.tscn")
-var interactable_prop_const = load("res://addons/vsk_entities/vsk_interactable_prop.tscn")
+const interactable_prop_const = preload("res://addons/vsk_entities/vsk_interactable_prop.tscn")
+const interactable_prop_const2 = preload("res://addons/vsk_entities/vsk_test_entity.tscn")
+const interactable_prop_const3 = preload("res://vsk_default/scenes/prefabs/beachball.tscn")
+# interactable_prop.tscn")
 
 var spawned_balls: Array = []
 
@@ -11,17 +13,22 @@ var spawned_balls: Array = []
 
 var spawn_key_pressed_last_frame: bool = false
 
-
 func spawn_ball_master(p_requester_id, _entity_callback_id: int) -> void:
 	print("Spawn ball master")
 
 	var requester_player_entity: RefCounted = VSKNetworkManager.get_player_instance_ref(p_requester_id)  # EntityRef
-
+	#var requester_player_entity2 = VSKNetworkManager.get_player_instance_ref(NetworkManager.get_current_peer_id())
+	
+	var spawn_model=load("res://vsk_default/scenes/prefabs/beachball.tscn")
 	if requester_player_entity:
+		var requester_transform = requester_player_entity.get_last_transform()
+		requester_transform.origin.z += 8 + randi_range(1, 10)
+		print(requester_player_entity.get_last_transform())
+		print(str(spawn_model))
 		if (
 			(EntityManager.spawn_entity(
-				interactable_prop_const,
-				{"transform": requester_player_entity.get_last_transform(), "model_scene": spawn_model},
+				interactable_prop_const2,
+				{"transform": requester_transform, "model_scene": spawn_model},
 				"NetEntity",
 				p_requester_id
 			))
@@ -42,8 +49,8 @@ func test_spawning() -> void:
 	print("test spawning")
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	var current_seconds = int(Time.get_unix_time_from_system()) % 60
-	if current_seconds % 2 == 0:
-		print("The current seconds are even!")
+	if current_seconds % 4 == 0:
+		print("The current seconds are double even!")
 		self.spawn_ball()
 
 	if InputManager.ingame_input_enabled():
