813bc2f8bf5634e018ea757c5e566c523782397c
dragonhunt02 <jackhunt43@libero.it>
2025-05-30 21:11:51 +0200

    Update test_entity_rpc_table.gd

--- addons/vsk_entities/extensions/test_entity_rpc_table.gd
+++ addons/vsk_entities/extensions/test_entity_rpc_table.gd
@@ -12,7 +12,7 @@ signal session_puppet_spawn(p_entity_callback_id)
 
 @rpc("any_peer") func spawn_prop(p_entity_callback_id, p_prop_scene) -> void:
 	if NetworkManager.is_session_master():
-		session_master_spawn.emit(get_remote_sender_id(), p_entity_callback_id)
+		session_master_spawn.emit(get_remote_sender_id(), p_entity_callback_id, p_prop_scene)
 	else:
 		if get_remote_sender_id() == NetworkManager.get_current_peer_id():
-			session_puppet_spawn.emit(p_entity_callback_id)
+			session_puppet_spawn.emit(p_entity_callback_id, p_prop_scene)
