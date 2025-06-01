ef06cd557e393dcc4cf6fa4cc503faccb81c0b72
dragonhunt02 <jackhunt43@libero.it>
2025-06-01 15:21:52 +0200

    Update vsk_menu_btn_animation.gd

--- addons/vsk_menu/vsk_menu_btn_animation.gd
+++ addons/vsk_menu/vsk_menu_btn_animation.gd
@@ -1,1 +1,30 @@
+extends Node
 
+@export var button_nodepath: NodePath = NodePath()
+
+@export var transition_time : float = 0.2 # seconds
+
+var active_tween: Tween = null
+
+func _on_focus_entered():
+	print("Starting scale: ", get_parent().scale)
+	var button_node = get_node_or_null(button_nodepath)
+
+	button_node.scale = Vector2(1.0, 1.0) # reset
+	button_node.pivot_offset = self.get_parent().size / 2
+
+	if active_tween:
+		active_tween.kill()
+		active_tween = null
+
+	active_tween = get_tree().create_tween() \
+		.set_trans(Tween.TRANS_SINE) \
+		.set_ease(Tween.EASE_IN_OUT)
+	active_tween.tween_property(button_node,"scale", Vector2(0.9, 0.9), trans_time)
+	active_tween.chain() \
+	.tween_property(button_node,"scale", Vector2(1.0,1.0), trans_time)
+	await active_tween.finished
+
+	#active_tween.kill()
+	button_node.scale = Vector2(1.0, 1.0)
+	button_node.release_focus()
