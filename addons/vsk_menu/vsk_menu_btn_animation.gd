5d856dd1e5af09d3cac5bf2d186a4a1726716c2a
dragonhunt02 <jackhunt43@libero.it>
2025-06-01 15:42:06 +0200

    Update vsk_menu_btn_animation.gd

--- addons/vsk_menu/vsk_menu_btn_animation.gd
+++ addons/vsk_menu/vsk_menu_btn_animation.gd
@@ -1,30 +1,29 @@
 extends Node
 
-@export var button_nodepath: NodePath = NodePath()
+#@export var button_nodepath: NodePath = NodePath()
 
 @export var transition_time : float = 0.2 # seconds
 
 var active_tween: Tween = null
 
-func _on_focus_entered():
-	print("Starting scale: ", get_parent().scale)
-	var button_node = get_node_or_null(button_nodepath)
+func animate_click(button_node : Node):
+	#print("Starting scale: ", get_parent().scale)
+	#var button_node = get_node_or_null(button_nodepath)
 
 	button_node.scale = Vector2(1.0, 1.0) # reset
-	button_node.pivot_offset = self.get_parent().size / 2
+	button_node.pivot_offset = button_node.size / 2
 
 	if active_tween:
 		active_tween.kill()
 		active_tween = null
 
-	active_tween = get_tree().create_tween() \
+	active_tween = button_node.create_tween() \
 		.set_trans(Tween.TRANS_SINE) \
 		.set_ease(Tween.EASE_IN_OUT)
-	active_tween.tween_property(button_node,"scale", Vector2(0.9, 0.9), trans_time)
+	active_tween.tween_property(button_node,"scale", Vector2(0.9, 0.9), transition_time)
 	active_tween.chain() \
-	.tween_property(button_node,"scale", Vector2(1.0,1.0), trans_time)
+	.tween_property(button_node,"scale", Vector2(1.0,1.0), transition_time)
 	await active_tween.finished
 
-	#active_tween.kill()
 	button_node.scale = Vector2(1.0, 1.0)
 	button_node.release_focus()
