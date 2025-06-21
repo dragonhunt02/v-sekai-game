3c1e031f25cfb4b140054efa3cd7e7e741e6a3c7
dragonhunt02 <jackhunt43@libero.it>
2025-06-21 06:58:25 +0200

    Update vsk_ui_view_account_action.gd

--- addons/vsk_ui/views/vsk_ui_view_account_action.gd
+++ addons/vsk_ui/views/vsk_ui_view_account_action.gd
@@ -7,11 +7,18 @@ class_name VSKUIViewAccountAction
 @export var _sign_in_content: Control = null
 @export var _register_content: Control = null
 
+@export var _register_email_field: LineEdit = null
+@export var _register_username_field: LineEdit = null
+@export var _register_password_field: LineEdit = null
+@export var _register_repeat_password_field: LineEdit = null
+@export var _register_submit_button: BaseButton = null
+
 @export var _sign_in_email_or_username_field: LineEdit = null
 @export var _sign_in_password_field: LineEdit = null
 @export var _sign_in_submit_button: BaseButton = null
 
 signal sign_in_selected
+signal sign_up_selected
 signal pick_other_domain_selected
 
 func _pick_other_domain_selected() -> void:
@@ -56,6 +63,9 @@ func _on_sign_in_password_text_changed(_new_text: String) -> void:
 
 func _on_sign_in_button_pressed() -> void:
 	sign_in_selected.emit()
+
+func _on_register_button_pressed() -> void:
+	sign_up_selected.emit()
 
 func _ready() -> void:
 	if not Engine.is_editor_hint():
@@ -108,4 +118,20 @@ func get_sign_in_username_or_email() -> String:
 ## Returns the text in the password sign in field.
 func get_sign_in_password() -> String:
 	return _sign_in_password_field.text
+	
+## Returns the text in the email registration field.
+func get_register_email() -> String:
+	return _register_email_field.text
+	
+## Returns the text in the username registration field.
+func get_register_username() -> String:
+	return _register_username_field.text
+	
+## Returns the text in the password registration field.
+func get_register_password() -> String:
+	return _register_password_field.text
+	
+## Returns the text in the repeat password registration field.
+func get_register_repeat_password() -> String:
+	return _register_repeat_password_field.text
 	
