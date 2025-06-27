6d9305e85dcea90a4367c094f90cc977482a61c1
dragonhunt02 <jackhunt43@libero.it>
2025-06-27 12:33:03 +0200

    Update vsk_game_service_uro.gd

--- addons/vsk_game_framework/scripts/managers/game_service_manager/vsk_game_service_uro.gd
+++ addons/vsk_game_framework/scripts/managers/game_service_manager/vsk_game_service_uro.gd
@@ -264,10 +264,8 @@ func _init() -> void:
 ## we are signed in with. On failure it will return a dictionary with an empty username
 ## and domain.
 func get_current_username_and_domain() -> Dictionary[String, String]:
-	var result_dictionary: Dictionary[String, String] = {"username": "", "domain": ""}
-	var account_address = _godot_uro.get_current_account_address()
-	if not account_address.empty():
-		result_dictionary = GodotUroHelper.get_username_and_domain_from_address(account_address)
+	var account_address: String = _godot_uro.get_current_account_address()
+	var result_dictionary: Dictionary[String, String] = GodotUroHelper.get_username_and_domain_from_address(account_address)
 	return result_dictionary
 
 ## Returns a string containing the currently active user account and domain
