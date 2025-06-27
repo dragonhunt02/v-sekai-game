c596e0fab3f62fd65db89cabe2cf9eef998e949e
dragonhunt02 <jackhunt43@libero.it>
2025-06-27 17:47:17 +0200

    Update script_utilities.gd

--- addons/sar_game_framework/generic/helpers/script_utilities.gd
+++ addons/sar_game_framework/generic/helpers/script_utilities.gd
@@ -20,14 +20,57 @@ static func does_script_inherit(
 			
 	return false
 
+const SENTINEL = Object.new()
+
 ## Compares two parameters and prints error message parameter if false.
 ## Used as replacement to assert() in exported projects.
-static func error_check(
+# assert_bool
+static func check_neq(
 	p_value: Variant,
 	p_expected: Variant,
-	p_error_msg: String = ""
+	p_error_msg: String = "error_check() failed"
 ) -> bool:
 	var result: bool = p_value == p_expected
+	if not result:
+		push_error("Script Error: " + p_error_msg)
+		# Editor debugger only
+		print_stack()
+		assert(result)
+
+	return result
+
+static func check_false(
+	p_value: Variant,
+	p_error_msg: String = "error_check_bool() failed!"
+) -> bool:
+	var result: bool = bool(p_value)
+	if not result:
+		push_error("Script Error: " + p_error_msg)
+		# Editor debugger only
+		print_stack()
+		assert(result)
+
+	return result
+
+static func check_null(
+	p_value: Variant,
+	p_error_msg: String = "Unexpected null value"
+) -> bool:
+	var result: bool = true
+	if p_value == null:
+		result = false
+		push_error("Script Error: " + p_error_msg)
+		# Editor debugger only
+		print_stack()
+		assert(result)
+
+	return result
+
+static func check_error(
+	p_value: Error,
+	p_error_msg: String = "Unexpected Error value"
+) -> bool:
+	var result: Error == OK
 	if not result:
 		push_error("Script Error: " + p_error_msg)
 		# Editor debugger only
