cc64d9dfc4a4f5a92c426f0803f9eee1d35070d1
dragonhunt02 <jackhunt43@libero.it>
2025-06-28 11:06:37 +0200

    Update sar_game_entity_3d.gd

--- addons/sar_game_framework/3d/entities/sar_game_entity_3d.gd
+++ addons/sar_game_framework/3d/entities/sar_game_entity_3d.gd
@@ -143,11 +143,13 @@ func _get_configuration_warnings() -> PackedStringArray:
 	var valid_path: String = get_game_entity_valid_scene_path()
 	if not valid_path.is_empty():
 		if not _is_inherited_base_scene(self, valid_path):
-			assert(string_array.append("The script for this GameEntity must be attached to scene derived from %s." % valid_path) == false)
-		
+			string_array.append("The script for this GameEntity must be attached to scene derived from %s." % valid_path)
+			VSKUtils.assert_true(false)
+
 		if not FileAccess.file_exists(get_game_entity_valid_scene_path()):
-			assert(string_array.append("Entity file path %s does not exist." % get_game_entity_valid_scene_path()) == false)
-		
+			string_array.append("Entity file path %s does not exist." % get_game_entity_valid_scene_path())
+			VSKUtils.assert_true(false)
+
 	# Interface assignment check
 	if not game_entity_interface:
 		assert(string_array.append("Game Entity interface has not been assigned") == false)
