0bc856ad0d538dea08869612e0fc178b80e57f99
dragonhunt02 <jackhunt43@libero.it>
2025-06-28 08:05:16 +0200

    Update vsk_ui_view_avatar_menu.gd

--- addons/vsk_ui/views/vsk_ui_view_avatar_menu.gd
+++ addons/vsk_ui/views/vsk_ui_view_avatar_menu.gd
@@ -30,7 +30,7 @@ func _fetch_content() -> void:
 							url)
 							
 						var avatar_url: String = "uro://" + dict.get("domain", "") + "/" + avatar.get("id", "")
-						if not VSKUtils.assert_ok(button.pressed.connect(_content_selected.bind(avatar_url))),
+						if not VSKUtils.assert_ok(button.pressed.connect(_content_selected.bind(avatar_url)),
 							"Could not connect signal 'button.pressed' to '_content_selected'")
 							return
 						assert(button.pressed.connect(_content_selected.bind(avatar_url)) == OK)
