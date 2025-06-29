56f632382b5fabfe01c32a0575f52d872f803f0b
dragonhunt02 <jackhunt43@libero.it>
2025-06-29 10:49:32 +0200

    Update sar_player_simulation_character_motor_component_3d.gd

--- addons/sar_game_framework/3d/simulation/sar_player_simulation_character_motor_component_3d.gd
+++ addons/sar_game_framework/3d/simulation/sar_player_simulation_character_motor_component_3d.gd
@@ -14,11 +14,11 @@ var _current_velocity: Vector3 = Vector3()
 	
 func _get_forward_direction() -> Basis:
 	var game_entity_interface: SarGameEntityInterface3D = simulation.get_game_entity_interface()
-	if not VSKUtils.assert_true(game_entity_interface, "SarSimulationComponentMotor3D: game_entity_interface is not available"):
+	if not VSKUtils.assert_true(game_entity_interface, "SarSimulationComponentMotor3D._get_forward_direction: game_entity_interface is not available"):
 		return Basis.IDENTITY
 
 	var game_entity: SarGameEntity3D = game_entity_interface.get_game_entity()
-	if not VSKUtils.assert_true(game_entity, "SarSimulationComponentMotor3D: game_entity is not available"):
+	if not VSKUtils.assert_true(game_entity, "SarSimulationComponentMotor3D._get_forward_direction: game_entity is not available"):
 		return Basis.IDENTITY
 	
 	return game_entity.global_transform.basis
@@ -57,7 +57,7 @@ func _process_movement(p_delta: float, p_movement_vector: Vector2) -> void:
 	var desired_direction: Vector3 = (_get_forward_direction() * movement_dir).normalized()
 	
 	var game_entity_interface: SarGameEntityInterface3D = simulation.get_game_entity_interface()
-	if not VSKUtils.assert_true(game_entity_interface, "SarSimulationComponentMotor3D: game_entity_interface is not available"):
+	if not VSKUtils.assert_true(game_entity_interface, "SarSimulationComponentMotor3D._process_movement: game_entity_interface is not available"):
 		return
 
 	_current_velocity = _movement_component.get_velocity()
