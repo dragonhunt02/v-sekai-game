3983519cfcf5f806abaaf1ce2a45ba425b1ade9a
dragonhunt02 <jackhunt43@libero.it>
2025-07-01 14:29:20 +0200

    Update vsk_ui_view_controller_big_menu.gd

--- addons/vsk_ui/view_controllers/vsk_ui_view_controller_big_menu.gd
+++ addons/vsk_ui/view_controllers/vsk_ui_view_controller_big_menu.gd
@@ -18,7 +18,7 @@ func _on_avatars_content_selected(p_url: String) -> void:
 signal message_box_requested(p_title: String, p_body: String)
 
 func show_messagebox(p_title: String, p_body: String) -> void:
-	if not VSKUtils.assert_true(message_box_requested.has_connections(), "Signal 'message_box_requested' has no connected callbacks")
+	if not VSKUtils.assert_true(message_box_requested.has_connections(), "Signal 'message_box_requested' has no connected callbacks"):
 		return
 
 	message_box_requested.emit(p_title, p_body)
