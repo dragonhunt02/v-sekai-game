03aeea2eced195996d6333364bec2347735e4791
dragonhunt02 <jackhunt43@libero.it>
2025-07-02 11:55:36 +0200

    Update build.sh

--- docker/build-project/build.sh
+++ docker/build-project/build.sh
@@ -10,6 +10,8 @@ fi
 
 git clone ${CLONE_OPTS} "https://github.com/${INPUT_REPO}.git" "./source"
 
+cd ./source; git merge rx-assert; cd ..;
+
 if [ -n "$INPUT_GAME_NAME" ]; then
     GAME_NAME=$INPUT_GAME_NAME;
 else
