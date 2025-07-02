83f4c18f75d6c32f96eb806f41bc5e43a22d779f
dragonhunt02 <jackhunt43@libero.it>
2025-07-02 16:10:10 +0200

    Update build.sh

--- docker/build-project/build.sh
+++ docker/build-project/build.sh
@@ -15,10 +15,10 @@ cd ./source; git merge origin/rx-assert; cd ..;
 #find ./source -type f -name "*.ttf.import*" -exec rm {} \;
 #####
 mkdir mytest; cd mytest;
-curl -OL https://github.com/godotengine/godot-builds/releases/download/4.5-beta2/Godot_v4.5-beta2_linux.x86_64.zip
-unzip Godot_v4.5-beta2_linux.x86_64.zip;rm Godot_v4.5-beta2_linux.x86_64.zip;
+#curl -OL https://github.com/godotengine/godot-builds/releases/download/4.5-beta2/Godot_v4.5-beta2_linux.x86_64.zip
+#unzip Godot_v4.5-beta2_linux.x86_64.zip;rm Godot_v4.5-beta2_linux.x86_64.zip;
 ls -a
-cp -v -f * ../${GODOT_EDITOR}
+#cp -v -f * ../${GODOT_EDITOR}
 cd ..
 ls -a
 #######
