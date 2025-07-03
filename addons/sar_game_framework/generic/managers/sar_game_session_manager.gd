459ad56d0669d51dc1ecc47306ce20d9c41dce7b
dragonhunt02 <jackhunt43@libero.it>
2025-07-03 17:47:44 +0200

    Update sar_game_session_manager.gd

--- addons/sar_game_framework/generic/managers/sar_game_session_manager.gd
+++ addons/sar_game_framework/generic/managers/sar_game_session_manager.gd
@@ -9,6 +9,18 @@ const _SHOW_WINDOW_TITLE_DEBUG_INFO_PATH: String = "game/session/show_window_tit
 const _PLAYER_SOUL_SCENE_PROJECT_SETTING_PATH: String = "game/session/player_soul_scene_path"
 const _PLAYER_VESSEL_SCENE_PROJECT_SETTING_PATH: String = "game/session/player_vessel_scene_path"
 
+const _DEFAULT_HOST_ARGS: Dictionary[String, Variant] = {
+	"map": "",
+	"server_name": "V-Sekai Server",
+	"port": 7777,
+	"ip": "127.0.0.1",
+	"dedicated": false,
+	"public": false,
+	"max_players": 64,
+}
+
+var _startup_network_opts: Dictionary = {}
+
 var _is_dedicated: bool = false
 var _max_players: int = 0
 
