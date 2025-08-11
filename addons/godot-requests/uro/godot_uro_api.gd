833b2f6c2cbad42aa080d403579750d13dc3c918
dragonhunt02 <jackhunt43@libero.it>
2025-08-11 02:03:50 +0200

    Update godot_uro_api.gd

--- addons/godot-requests/uro/godot_uro_api.gd
+++ addons/godot-requests/uro/godot_uro_api.gd
@@ -396,19 +396,6 @@ func dashboard_get_map_async(p_requester: GodotUroRequester, p_access_token: Str
 
 	return _handle_result(result)
 
-static func _handle_result(result: RefCounted) -> Dictionary:
-	var result_dict: Dictionary = {
-		"requester_code": -1, "generic_code": -1, "response_code": -1, "output": {}
-	}
-
-	if result:
-		result_dict["requester_code"] = result.requester_code
-		result_dict["generic_code"] = result.generic_code
-		result_dict["response_code"] = result.response_code
-		result_dict["output"] = result.data
-
-	return result_dict
-
 
 func _init(p_godot_uro):
 	_godot_uro = p_godot_uro
