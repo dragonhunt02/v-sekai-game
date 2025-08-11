7f5dceee65cda6e5285dc3e85e1d7d0e0b192915
dragonhunt02 <jackhunt43@libero.it>
2025-08-11 05:44:39 +0200

    Create godot_vroid_api.gd

---
+++ addons/godot-requests/vroid/godot_vroid_api.gd
@@ -0,0 +1,133 @@
+# Copyright (c) 2018-present. This file is part of V-Sekai https://v-sekai.org/.
+# SaracenOne & K. S. Ernest (Fire) Lee & Lyuma & MMMaellon & Contributors
+# godot_vroid_api.gd
+# SPDX-License-Identifier: MIT
+
+@tool
+extends GodotRequestAPI
+class_name GodotVroidAPI
+
+var _godot_vroid: GodotVroid = null
+
+func _init(p_godot_vroid):
+	_godot_vroid = p_godot_vroid
+
+func get_profile_async(p_requester: GodotRequester, p_access_token: String) -> Dictionary:
+	var query: Dictionary = {}
+
+	var result = await (p_requester.request(
+		GodotVroidHelper.get_api_path() + GodotVroidHelper.PROFILE_PATH,
+		query,
+		p_access_token,
+		{"method": HTTPClient.METHOD_GET, "encoding": "query"}
+	))
+
+	return _handle_result(result)
+
+
+func get_uploaded_avatars_async(p_requester: GodotRequester, p_access_token: String, p_filter: Dictionary = {}, p_max_id: String = "", p_count: int = -1) -> Dictionary:
+	var query: Dictionary = {}
+
+	const filter = GodotVroidHelper.interpolate_default_model_filter(p_filter)
+	query = filter
+	if p_max_id != "":
+		query["max_id"] = p_max_id
+	if p_count >= 1:
+		count = min(p_count, 100) # clamp
+	else:
+		count = 20 # default
+	query["count"] = count
+
+	var result = await (p_requester.request(
+		GodotVroidHelper.get_api_path() + GodotVroidHelper.UPLOADED_MODELS_PATH,
+		query,
+		p_access_token,
+		{"method": HTTPClient.METHOD_GET, "encoding": "query"}
+	))
+
+	return _handle_result(result)
+
+
+func get_liked_avatars_async(p_requester: GodotRequester, p_access_token: String, p_filter: Dictionary = {}, p_max_id: String = "", p_count: int = -1) -> Dictionary:
+	var query: Dictionary = {}
+
+	const filter = GodotVroidHelper.interpolate_default_model_filter(p_filter)
+	query = filter
+	if p_max_id != "":
+		query["max_id"] = p_max_id
+	if p_count >= 1:
+		count = min(p_count, 100) # clamp
+	else:
+		count = 20 # default
+	query["count"] = count
+
+	var result = await (p_requester.request(
+		GodotVroidHelper.get_api_path() + GodotVroidHelper.HEARTS_PATH,
+		query,
+		p_access_token,
+		{"method": HTTPClient.METHOD_GET, "encoding": "query"}
+	))
+
+	return _handle_result(result)
+
+
+func get_staff_picks_async(p_requester: GodotRequester, p_access_token: String, p_max_id: String = "", p_count: int = -1) -> Dictionary:
+	var query: Dictionary = {}
+
+	if p_max_id != "":
+		query["max_id"] = p_max_id
+	if p_count >= 1:
+		count = min(p_count, 100) # clamp
+	else:
+		count = 20 # default
+	query["count"] = count
+
+	var result = await (p_requester.request(
+		GodotVroidHelper.get_api_path() + GodotVroidHelper.STAFF_PICKS_PATH,
+		query,
+		p_access_token,
+		{"method": HTTPClient.METHOD_GET, "encoding": "query"}
+	))
+
+	return _handle_result(result)
+
+
+func search_models_async(p_requester: GodotRequester, p_access_token: String, keyword: String, p_filter: Dictionary = {}, p_search_after: String = "", p_sort: String = "", p_count: int = -1) -> Dictionary:
+	var query: Dictionary = {}
+
+	const filter = GodotVroidHelper.interpolate_default_model_filter(p_filter)
+	query = filter
+	if p_search_after != "":
+		query["search_after[]"] = p_search_after
+	if p_sort != "":
+		query["sort"] = p_sort
+	if p_count >= 1:
+		count = min(p_count, 100) # clamp
+	else:
+		count = 20 # default
+	query["count"] = count
+
+	var result = await (p_requester.request(
+		GodotVroidHelper.get_api_path() + GodotVroidHelper.SEARCH_PATH,
+		query,
+		p_access_token,
+		{"method": HTTPClient.METHOD_GET, "encoding": "query"}
+	))
+
+	return _handle_result(result)
+
+
+func get_model_details_async(p_requester: GodotRequester, p_access_token: String, p_id: String ) -> Dictionary:
+	var query: Dictionary = {}
+
+	query["id"] = p_id
+
+	var result = await (p_requester.request(
+		GodotVroidHelper.get_api_path() + GodotVroidHelper.MODEL_PATH + "/" + p_id,
+		query,
+		p_access_token,
+		{"method": HTTPClient.METHOD_GET, "encoding": "query"}
+	))
+
+	return _handle_result(result)
+
