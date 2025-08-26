beb26a0795e6114daaf496d41dcb0203775a2069
dragonhunt02 <jackhunt43@libero.it>
2025-08-26 22:46:08 +0200

    Update vsk_settings_info.gd

--- addons/vsk_game_framework/scripts/data/vsk_settings_info.gd
+++ addons/vsk_game_framework/scripts/data/vsk_settings_info.gd
@@ -6,6 +6,9 @@ class_name VSKSettingsInfo
 
 @export var settings: Array[VSKSettingsInfoSetting] = []
 
+func _init() -> void:
+	print("ok")
+
 func get_category(p_category: String) -> Array:
 	var result: Array = []
 	for setting in settings:
@@ -18,14 +21,20 @@ func get_categories_dict() -> Dictionary:
 	var category: String = ""
 	for setting in settings:
 		category = setting.category
-		result[category] = setting
+		if not result.get(category):
+			result[category] = []
+		result[category].append(setting)
 	return result
 
-# Assuming keys are unique across categories
-func get_key(p_key, p_default = null) -> Dictionary:
-	var result: Dictionary = p_default
+func get_key(p_section: String, p_key: String, p_default = null) -> VSKSettingsInfoSetting:
+	var result = p_default
 	for setting in settings:
-		if setting.key == p_key:
+		if setting.category == p_section and setting.key == p_key:
 			return setting
 	return result
 
+func to_array() -> Array:
+	return settings.duplicate(true)
+
+func _to_string() -> String:
+	return str(self.settings)
