22766cd10de50bf2af793e0bfd2ead450420ccda
dragonhunt02 <jackhunt43@libero.it>
2025-08-26 16:59:21 +0200

    Update vsk_settings_info_setting.gd

--- addons/vsk_game_framework/scripts/data/vsk_settings_info_setting.gd
+++ addons/vsk_game_framework/scripts/data/vsk_settings_info_setting.gd
@@ -2,4 +2,9 @@
 extends Resource
 class_name VSKSettingsInfoSetting
 
-@export var path: String = ""
+@export var display_name: String
+@export var category: String
+@export var key: String
+@export var type: Variant.Type
+@export var hint: PropertyHint
+@export var hint_string: String
